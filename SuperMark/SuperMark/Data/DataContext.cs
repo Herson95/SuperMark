f4a566c0834448e1bb3f8c1b6fe21184c3fc71d2
Herson <Hersonprez@gmail.com>
2019-11-27 20:09:18 -0600

    Create Entities

---
+++ SuperMark/SuperMark/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿namespace SuperMark.Data
+{
+    using Entities;
+    using Microsoft.EntityFrameworkCore;
+
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
